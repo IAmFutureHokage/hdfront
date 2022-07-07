5b9dbaf458b7054592e76d63e10d78f9d0489d99
IAmFutureHokage <105332106+IAmFutureHokage@users.noreply.github.com>
2022-07-08 09:57:43 +1000

    Update new.component.ts

--- src/app/requests/new/new.component.ts
+++ src/app/requests/new/new.component.ts
@@ -21,7 +21,6 @@ export class NewComponent implements OnInit {
   public description: string | undefined;
   public chennal = "";
   public infsys = "";
-  private valid = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
 
   constructor(
     private fieldsService : FieldsService,
@@ -40,11 +39,8 @@ export class NewComponent implements OnInit {
     if(this.theme === undefined){this.theme = ""}
     if(this.description === undefined){this.description = ""}
     if(this.a_mail === undefined){this.a_mail = ""}
-    if((this.valid.test(this.a_mail)||(this.a_mail === "")
-    )){
     this.requestsService.addRequest(this.a_name, this.a_mail, this.theme, this.description, this.chennal, this.infsys)
     .then((response) => {this.router.navigate(['requests']);})
     .execute();
-    }
   }
 }
